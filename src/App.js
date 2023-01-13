430f9df9a9bdb4cf42e1eaf599441fc0e05c9710
abc <MY_NAME@example.com>
2023-01-13 20:43:47 +0500

    Geolocation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
+  const [location, setLocation] = React.useState('')
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, getError);
+    } else {
+     setLocation("Geolocation is not supported by this browser.")
+    }
+  }
+  
+  function showPosition(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setLocation(`https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`)
+  }
+
+  function getError() {
+    setLocation('Unable to retrieve your location') ;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+        <div className="App">
+            <a className='map-link' href={location} target='_blank' rel="noreferrer">{location}</a>
+            <button className='locator-button' onClick={getLocation}>Locate</button>
+        </div>
   );
 }
 
